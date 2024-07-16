42cea90ac152f9abdff01f1813f38ed53b98954e
HSHmeng <141244101+hshmeng@users.noreply.github.com>
2024-07-16 15:19:29 +0800

    增加了更多更精准的设备检验

---
+++ VEC_PAGE_0001_HOME/INDEX/DEVICE.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var userAgent = navigator.userAgent;
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
+        window.location.href = "INDEX_MOVE.html";
+    } else {
+        window.location.href = "INDEX_HOST.html";
+    }
+}
