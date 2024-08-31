cfe9d3259c51a75083f122c29f69495d43e98f9c
HSHMENG <141244101+hshmeng@users.noreply.github.com>
2024-08-31 11:33:40 +0800

    更新了公开数据

--- VEC_PAGE_0001_HOME/INDEX/OFFICE.js
+++ VEC_PAGE_0001_HOME/INDEX/OFFICE.js
@@ -1,7 +1,16 @@
-document.getElementById('link').addEventListener('click', function(e) {
+document.getElementById('link1').addEventListener('click', function(e) {
     e.preventDefault();
-    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('overlay1').style.display = 'block';
 });
-document.getElementById('close').addEventListener('click', function() {
-    document.getElementById('overlay').style.display = 'none';
+document.getElementById('link2').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('overlay2').style.display = 'block';
 });
+document.getElementById('link3').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('overlay3').style.display = 'block';
+});
+
+function closeOverlay(id) {
+    document.getElementById(id).style.display = 'none';
+}
