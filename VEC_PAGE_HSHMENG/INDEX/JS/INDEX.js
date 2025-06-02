37bb362a3387001ed63c34995a1d8c4c306a6c7f
HSHMENG <141244101+hshmeng@users.noreply.github.com>
2025-06-03 03:24:33 +0800

    删除不需要的代码

--- VEC_PAGE_HSHMENG/INDEX/JS/INDEX.js
+++ VEC_PAGE_HSHMENG/INDEX/JS/INDEX.js
@@ -12,39 +12,3 @@ document.addEventListener('mousemove', (e) => {
     document.body.style.background = `radial-gradient(circle ${size}px at ${x}% ${y}%, #ff9e59 0%, #ffe3c1 100%)`;
 });
 
-//实现MP3播放
-document.getElementById('playButton').addEventListener('click', function() {
-    var audio = document.getElementById('audioPlayer');
-    var progressContainer = document.getElementById('progressContainer');
-    var progressBar = document.getElementById('progressBar');
-
-    audio.volume = 1.0; // 设置音量为最大
-    audio.play();
-
-    progressContainer.style.display = 'block';
-
-    audio.addEventListener('timeupdate', function() {
-        var progress = (audio.currentTime / audio.duration) * 100;
-        progressBar.style.width = progress + '%';
-    });
-
-    progressBar.addEventListener('mousedown', function(e) {
-        var onMouseMove = function(e) {
-            var rect = progressContainer.getBoundingClientRect();
-            var offsetX = e.clientX - rect.left;
-            var newTime = (offsetX / rect.width) * audio.duration;
-            audio.currentTime = newTime;
-        };
-
-        document.addEventListener('mousemove', onMouseMove);
-
-        document.addEventListener('mouseup', function() {
-            document.removeEventListener('mousemove', onMouseMove);
-        }, { once: true });
-    });
-
-    audio.addEventListener('ended', function() {
-        progressContainer.style.display = 'none';
-        progressBar.style.width = '0%';
-    });
-});
